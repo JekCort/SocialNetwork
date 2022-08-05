c2f06334005fb010086a4b05b999d78629e81b7b
Yevgen Kostenko <jekcort969@gmail.com>
2022-08-05 16:19:13 +0300

    less18 new page Dialogs and changing styles

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,14 +4,14 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img
                     src="https://3dwarehouse.sketchup.com/warehouse/v1.0/content/public/8fc6bacc-297b-40c4-9b9f-ce77a75cc55a"
                     alt=""/>
             </div>
             <div>
-                ava + dis
+                ava + desc
             </div>
             <MyPosts/>
         </div>
