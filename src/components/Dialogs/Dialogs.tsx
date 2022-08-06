2143fb03e35039c71dd549c340f88bf44a48c45c
Yevgen Kostenko <jekcort969@gmail.com>
2022-08-06 13:38:31 +0300

    less21 layout of the dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,31 @@ import classes from './Dialogs.module.css';
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+
+            <div className={classes.dialogsItem}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Bob
+                </div>
+
+                <div className={classes.dialog}>
+                    Eva
+                </div>
+                <div className={classes.dialog}>
+                    Sveta
+                </div>
+                <div className={classes.dialog}>
+                    Viktor
+                </div>
+                <div className={classes.dialog}>
+                    Valera
+                </div>
             </div>
+            <div className={classes.messages}>
+                <div className={classes.massage}>Hi</div>
+                <div className={classes.massage}> How are a you</div>
+                <div className={classes.massage}>Yo</div>
+            </div>
+        </div>
     );
 };
