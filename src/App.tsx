00f3ca3e6c51c57db62471df969362dffde92ed3
Yevgen Kostenko <jekcort969@gmail.com>
2022-08-07 20:19:03 +0300

    less27 component vs render

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,59 @@ import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
 
 
+// type dialogPropsType = {
+//     id: number,
+//     name: string
+// }
+//
+// type dialogsPropsType = {
+//     dialogs: Array<dialogPropsType>
+// }
+//
+// type messagePropsType = {
+//     id: number,
+//     name: string
+// }
+//
+// type messagesPropsType = {
+//     messages: Array<messagePropsType>
+// }
+//
+// const dialogs = [
+//     {id: 1, name: "Bob"},
+//     {id: 2, name: "Eva"},
+//     {id: 3, name: "Sveta"},
+//     {id: 4, name: "Viktor"},
+//     {id: 5, name: "Valera"}
+// ]
+// const messages = [
+//     {id: 1, message: "Hi"},
+//     {id: 2, message: "How are a you"},
+//     {id: 3, message: "Yo"},
+//     {id: 4, message: "Yo"},
+//     {id: 5, message: "Yo"}
+// ]
+
+
+const HandlerDialogs = () => <Dialogs/>
+const HandlerProfile = () => <Profile/>
+
+
 const App = () => {
     return (
         <BrowserRouter>
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Route path="/DialogsItem" component={Dialogs}/>
-                <Route path="/Profile" component={Profile}/>
-                <Route path="/News" component={News}/>
-                <Route path="/Music" component={Music}/>
-                <Route path="/Settings" component={Settings}/>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path="/DialogsItem" render={HandlerDialogs}/>
+                    <Route path="/Profile" render={HandlerProfile}/>
+                    <Route path="/News" component={News}/>
+                    <Route path="/Music" component={Music}/>
+                    <Route path="/Settings" component={Settings}/>
+                </div>
             </div>
-        </div>
-            </BrowserRouter>
+        </BrowserRouter>
     );
 }
 
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 import classes from './Dialogs.module.css';
 import {DialogItem} from "./DialogItem/DialogItem";
 import { Message } from './Message/Message';
@@ -20,7 +19,6 @@ export const Dialogs = () => {
         {id: 4, message: "Yo"},
         {id: 5, message: "Yo"}
     ]
-
     const dialogsItems = dialogs
         .map(d => <DialogItem name={d.name} id={d.id}/>)
 
